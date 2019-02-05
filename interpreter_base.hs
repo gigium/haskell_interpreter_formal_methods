0435e704d8cab05a8f1f8569a8655fec21201508
federico-code <federicodelaurentiis7@gmail.com>
2019-02-05 18:55:16 +0100

    rename some command in base file

--- interpreter_base.hs
+++ interpreter_base.hs
@@ -8,29 +8,44 @@ import Control.Applicative -- Otherwise you can't do the Applicative instance.
 
 type Var = String
 
---Next, we define expressions.
+--Next, we define ArithmeticAexpressions.
 --Expressions are constructed from integer constants, variable references, and arithmetic operations.
 
 
-infixl 6 :+:, :-:
-infixl 7 :*:, :/:
+infix 1 `IS`
+infixl 6 `SUM`, `DIF`
+infixl 7 `MUL`, `DIV`
+infixl 8 `AND`, `OR`, `Not`
+infixl 9 `EQUAL`, `NOT_EQUAL`, `LESS_THAN`, `GREATER_THAN`,`GREATER_EQUAL_TO`, `LESS_EQUAL_TO`
 
-data Exp
+data ArithmeticAexp
   = C Int        -- constant                                                     
   | V Var        -- variable                                                     
-  | Exp :+: Exp  -- addition                                                     
-  | Exp :-: Exp  -- subtraction                                                  
-  | Exp :*: Exp  -- multiplication                                               
-  | Exp :/: Exp  -- division
+  | ArithmeticAexp `SUM` ArithmeticAexp  -- addition                                                     
+  | ArithmeticAexp `DIF` ArithmeticAexp  -- subtraction                                                  
+  | ArithmeticAexp `MUL` ArithmeticAexp  -- multiplication                                               
+  | ArithmeticAexp `DIV` ArithmeticAexp  -- division
+
+data BooleanAexp = Boolean Bool -- Constant
+ | B Var -- Variable
+ | BooleanAexp `AND` BooleanAexp -- Logic and
+ | BooleanAexp `OR` BooleanAexp -- Logic or
+ | Not BooleanAexp -- Negation
+ |ArithmeticAexp `EQUAL` ArithmeticAexp -- Equality between twoArithmeticAexp
+ |ArithmeticAexp `NOT_EQUAL` ArithmeticAexp --Inequality between two Axp
+-- Comparison amongArithmeticAexp
+ |ArithmeticAexp `LESS_THAN` ArithmeticAexp
+ |ArithmeticAexp `GREATER_THAN` ArithmeticAexp
+ |ArithmeticAexp `GREATER_EQUAL_TO` ArithmeticAexp
+ |ArithmeticAexp `LESS_EQUAL_TO` ArithmeticAexp
 
   --The statement language is rather minimal.
   --We have three forms of statements: variable assignments, while loops, and sequences.
 
-infix 1 :=
 
 data Stmt
-  = Var := Exp      -- assignment                                                
-  | While Exp Stmt  -- loop                                                      
+  = Var `IS` ArithmeticAexp      -- assignment                                                
+  | While ArithmeticAexp Stmt  -- loop                                                      
   | Seq [Stmt]      -- sequence
 
 
@@ -38,7 +53,7 @@ data Stmt
 
 type Prog = Stmt
 
---While running a program, we need to keep track of the values assigned to the different variables in the programs. 
+--While running a program, we need to keep track of the values assigned to the `dif`ferent variables in the programs. 
 --These values are just integers and as a representation of our "memory" we just use lists of pairs consisting of 
 --a variable and a value.
 
@@ -51,24 +66,24 @@ type Store = [(Var, Val)]
 -- Note that if the store contains multiple bindings for a variable, 
 --lookup selects the bindings that comes first in the store.
 
-eval :: Exp -> Store -> Val
+eval :: ArithmeticAexp -> Store -> Val
 eval (C n) r       = n
 eval (V x) r       = case lookup x r of
                        Nothing -> error ("unbound variable `" ++ x ++ "'")
                        Just v  -> v
-eval (e1 :+: e2) r = eval e1 r + eval e2 r
-eval (e1 :-: e2) r = eval e1 r - eval e2 r
-eval (e1 :*: e2) r = eval e1 r * eval e2 r
-eval (e1 :/: e2) r = eval e1 r `div` eval e2 r
+eval (e1 `SUM` e2) r = eval e1 r + eval e2 r
+eval (e1 `DIF` e2) r = eval e1 r - eval e2 r
+eval (e1 `MUL` e2) r = eval e1 r * eval e2 r
+eval (e1 `DIV` e2) r = eval e1 r `div` eval e2 r
 
--- While the evaluation of an expression cannot alter the contents of the store, executing a statement may in fact result in an update of the store. Hence,
+-- While the evaluation of an ArithmeticAexpression cannot alter the contents of the store, executing a statement may in fact result in an update of the store. Hence,
 -- the function for executing a statement takes a store as an argument and produces a possibly updated store.
 
 -- Note that, in the case of assignments, we simply push a new binding for the updated variable to the store,
 -- effectively shadowing any previous bindings for that variable.
 
 exec :: Stmt -> Store -> Store
-exec (x := e) r                    = (x, eval e r) : r
+exec (x `IS` e) r                    = (x, eval e r) : r
 exec (While e s) r | eval e r /= 0 = exec (Seq [s, While e s]) r
                    | otherwise     = r
 exec (Seq []) r                    = r
@@ -86,91 +101,13 @@ run p r = nubBy ((==) `on` fst) (exec p r)
 
 fib :: Prog
 fib = Seq
-  [ "x" := C 0
-  , "y" := C 1
+  [ "x" `IS` C 0
+  , "y" `IS` C 1
   , While (V "n") $ Seq
-      [ "z" := V "x" :+: V "y"
-      , "x" := V "y"
-      , "y" := V "z"
-      , "n" := V "n" :-: C 1
+      [ "z" `IS` V "x" `SUM` V "y"
+      , "x" `IS` V "y"
+      , "y" `IS` V "z"
+      , "n" `IS` V "n" `DIF` C 1
       ]
   ]
 
-
---We can use monad transformers to construct a composite monad for our two effects by combining a basic state monad and a basic error monad.
---Here, however, we simply construct the composite monad in one go.
-{-
-newtype Interp a = Interp { runInterp :: Store -> Either String (a, Store) }
-
-instance Monad Interp where
-  return x = Interp $ \r -> Right (x, r)
-  i >>= k  = Interp $ \r -> case runInterp i r of
-               Left msg      -> Left msg
-               Right (x, r') -> runInterp (k x) r'
-  fail msg = Interp $ \_ -> Left msg
-
--}
---to the imports and make Interp an instance of Functor and Applicative like this
-{-
-instance Functor Interp where
-  fmap = liftM -- imported from Control.Monad
-
-instance Applicative Interp where
-  pure  = return
-  (<*>) = ap -- imported from Control.Monad
--}
-
---For reading from and writing to the store, we introduce effectful functions rd and wr:
---Note that rd produces a Left-wrapped error message if a variable lookup fails.
-{-
-rd :: Var -> Interp Val
-rd x = Interp $ \r -> case lookup x r of
-         Nothing -> Left ("unbound variable `" ++ x ++ "'")
-         Just v  -> Right (v, r)
-
-wr :: Var -> Val -> Interp ()
-wr x v = Interp $ \r -> Right ((), (x, v) : r)
--}
-
---Note that rd produces a Left-wrapped error message if a variable lookup fails.
---The monadic version of the expression evaluator now reads
-{-
-eval :: Exp -> Interp Val
-eval (C n)       = do return n
-eval (V x)       = do rd x
-eval (e1 :+: e2) = do v1 <- eval e1
-                      v2 <- eval e2
-                      return (v1 + v2)
-eval (e1 :-: e2) = do v1 <- eval e1
-                      v2 <- eval e2
-                      return (v1 - v2)
-eval (e1 :*: e2) = do v1 <- eval e1
-                      v2 <- eval e2
-                      return (v1 * v2)
-eval (e1 :/: e2) = do v1 <- eval e1
-                      v2 <- eval e2
-                      if v2 == 0
-                        then fail "division by zero"
-                        else return (v1 `div` v2)
--}
-
---For the execution of statements we have
---The type of exec conveys that statements do not result in values but are executed only 
---for their effects on the store or the run-time errors they may trigger.
-{-
-exec :: Stmt -> Interp ()
-exec (x := e)       = do v <- eval e
-                         wr x v
-exec (While e s)    = do v <- eval e
-                         when (v /= 0) (exec (Seq [s, While e s]))
-exec (Seq [])       = do return ()
-exec (Seq (s : ss)) = do exec s
-                         exec (Seq ss)
--}
---Finally, in the function run we perform a monadic computation and process its effects.
-{-
-run :: Prog -> Store -> Either String Store
-run p r = case runInterp (exec p) r of
-            Left msg      -> Left msg
-            Right (_, r') -> Right (nubBy ((==) `on` fst) r')
--}            
