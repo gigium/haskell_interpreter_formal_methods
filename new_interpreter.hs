58cad9cc4c76a199384fd22422de1ae25bdb0be8
gigum <minervini.luigi1@gmail.com>
2019-03-23 14:56:10 +0100

    monadic parser translation from pure functional

--- new_interpreter.hs
+++ new_interpreter.hs
@@ -2,45 +2,19 @@ import           Control.Applicative
 import           Data.Char
 
 
-type Env = [(Char, Int)]
 
-newtype Parser a = P (Env -> String -> [(Env, a, String)])
 
-parse :: Parser a -> Env -> String -> [(Env, a, String)]
+newtype Parser a = P (String -> [(a, String)])
+
+parse :: Parser a -> String -> [(a, String)]
 parse (P p) inp = p inp
 
-instance Functor Parser where
-  -- fmap :: (a -> b) -> Parser a -> Parser b
-  fmap g p = P $ \env inp ->
-    case parse p env inp of
-      [] -> []
-      [(env, v,out)] -> [(env, g v, out)]
+item :: Parser Char
+item = P $ \inp ->
+  case inp of
+    []     -> []
+    (x:xs) -> [(x, xs)]
 
-instance Applicative Parser where
-  -- pure :: a -> Parser a
-  pure v = P (\env inp -> [(env, v,inp)])
-  -- <*> :: Parser (a -> b) -> Parser a -> Parser b
-  pg <*> px = P (\env inp -> case parse pg env inp of
-    [] -> []
-    [(env, g,out)] -> parse (fmap g px) env out)
-
-instance Monad Parser where
-  p >>= f = P (\env inp -> case parse p env inp of
-    [] -> []
-    [(env, v,out)] -> parse (f v) env out)
-  return a = P (\env cs -> [(env, a,cs)])
-
-
-instance Alternative Parser where
-  -- empty :: Parser a
-  empty = P $ \env inp -> []
-  -- (<|>) :: Parser a -> Parser a -> Parser a
-  p <|> q = P $ \env inp ->
-    case parse p  env inp of
-      []         -> parse q env inp
-      [(env,v, out)] -> [(env,v, out)]
-
--- we define a parser sat p for single characters that satisfy the predicate p:
 sat :: (Char -> Bool) -> Parser Char
 sat p = do
   x <- item
@@ -48,10 +22,23 @@ sat p = do
   then return x
   else empty
 
--- Using sat and appropriate predicates from the library Data.Char, we can now define parsers for
--- digits
-digit :: Parser Char
-digit = sat isDigit
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+token :: Parser a -> Parser a
+token p = do
+  space
+  v <- p
+  space
+  return v
+
+space :: Parser ()
+space = do
+  many (sat isSpace)
+  return ()
+
+
+
 
 -- lower-case letters
 lower :: Parser Char
@@ -69,12 +56,29 @@ letter = sat isAlpha
 alphanum :: Parser Char
 alphanum = sat isAlphaNum
 
--- specific characters
-char :: Char -> Parser Char
-char x = sat (== x)
+ident :: Parser String
+ident = do
+  x <- lower
+  xs <- many alphanum
+  return (x:xs)
 
--- using char we can define a parser string xs for the string of characters xs, with the string
--- itself returned as the result value
+-- Using token, we can now define parsers that
+-- ignore spacing around identifiers
+identifier :: Parser String
+identifier = token ident
+
+nat :: Parser Int
+nat = do
+  xs <- some digit
+  return (read xs)
+
+-- ignore spacing around natural numbers
+natural :: Parser Int
+natural = token nat
+
+digit :: Parser Char
+digit = sat isDigit
+
 string :: String -> Parser String
 string [] = return []
 string (x:xs) = do
@@ -82,359 +86,347 @@ string (x:xs) = do
   string xs
   return (x:xs)
 
-
--- Using many and some (already implemented in alternative), we can now define parsers for 
--- identifiers (variable names) comprising a lowercase letter followed by zero or more alphanumeric characters
-ident :: Parser String
-ident = do
-  x <- lower
-  xs <- many alphanum
-  return (x:xs)
-
- -- natural numbers comprising one or more digits
-nat :: Parser Int
-nat = do
-  xs <- some digit
-  return (read xs)
-
- -- and spacing comprising zero or more space, tab, and newline characters
-space :: Parser ()
-space = do
-  many (sat isSpace)
-  return ()
+char :: Char -> Parser Char
+char x = sat (== x)
 
 
--- using nat it is now straightforward to define a parser for integer values
-int :: Parser Int
-int = do
-  char '-'
-  n <- nat
-  return (-n) <|> nat
+true_keyword :: Parser String
+true_keyword = symbol "TRUE"
+
+false_keyword :: Parser String
+false_keyword = symbol "FALSE"
 
 
--- To handle spacing, we define a new primitive that ignores any space before and after applying a parser for a token
-token :: Parser a -> Parser a
-token p = do
-  space
-  v <- p
-  space
-  return v
+bool_val :: Parser Bool
+bool_val = do 
+  true_keyword
+  return (True)
+  <|> 
+  do 
+    false_keyword
+    return(False)
+  
 
--- Using token, we can now define parsers that
--- ignore spacing around identifiers
-identifier :: Parser String
-identifier = token ident
+instance Functor Parser where
+  fmap f p = P $ \inp ->
+    case parse p inp of
+      []         -> []
+      [(v, out)] -> [(f v, out)]
 
--- ignore spacing around natural numbers
-natural :: Parser Int
-natural = token nat
+instance Applicative Parser where
+  pure v = P $ \inp -> [(v, inp)]
 
--- ignore spacing around integers
-integer :: Parser Int
-integer = token int
+  pf <*> px = P $ \inp ->
+    case parse pf inp of
+      []         -> []
+      [(f, out)] -> parse (fmap f px) out
 
--- ignore spacing around special symbols
-symbol :: String -> Parser String
-symbol xs = token (string xs)
+instance Monad Parser where
+  p >>= f = P $ \inp ->
+    case parse p inp of
+      []         -> []
+      [(v, out)] -> parse (f v) out
+
+instance Alternative Parser where
+  empty = P $ \inp -> []
+
+  p <|> q = P $ \inp ->
+    case parse p inp of
+      []         -> parse q inp
+      [(v, out)] -> [(v, out)]
+
+-----------------------------------------------------------------------------
+
+data Operator = Plus | Minus | Times | Div 
+                | And | Or | Not 
+                | Greater | GreaterEqual | Less | LessEqual | Equal | NotEqual 
+                | While | If 
+    deriving (Show, Eq)
+
+--Definition of expression types -> nodes in the parser tree
+data Tree = SeqNode [Tree]
+          | StatementNode Tree 
+          | AssignNode String Tree
+          --arithmetic
+          | SumNode Operator Tree Tree
+          | ProdNode Operator Tree Tree
+          | UnaryNode Operator Tree
+          --boolean logic
+          | LogicNode Operator Tree Tree
+          | ArithmeticLogicNode Operator Tree Tree
+          | UnaryBoolNode Operator Tree
+          -- While, If
+          | CommandNode Operator Tree Tree
+          --terminals
+          | NumNode Int | BoolNode Bool | VarNode String          
+    deriving Show     
 
 
-item :: Parser Char
-item = P (\env inp -> case inp of
-  [] -> []
-  (x:xs) -> [(env, x,xs)])
 
-varValue :: Parser Int
-varValue = P(\env inp -> case inp of
-  [] -> []
-  (x:xs) -> [(env, getVarFun env x, xs)])
-
-
-getVarFun :: Env -> Char -> Int
-getVarFun [(a,b)] c = 3+3
+-----------------------------------------------------------------------------
+program :: Parser Tree
+program = do 
+  symbol "{"
+  s <-  sqnc
+  symbol "}"
+  return (SeqNode s)
 
 
-parseTrue :: Parser String
-parseTrue = do {
-  symbol "t";
-  symbol "r";
-  symbol "u";
-  symbol "e";
-  }
-
-parseFalse :: Parser String
-parseFalse = do {
-  symbol "f";
-  symbol "a";
-  symbol "l";
-  symbol "s";
-  symbol "e";
-  }
+stm :: Parser Tree
+stm = do
+    c <- comm
+    return (StatementNode c)
+    <|>
+    do
+      e <- expr
+      return (StatementNode e)
 
 
-aExpr :: Parser Int
-aExpr = do {
-  t <- term;
-  do {
-    symbol "+";
-    e <- aExpr;
-    return (t + e);
-    }
+
+
+sqnc :: Parser [Tree]
+sqnc = do 
+  s <- stm 
+  symbol ";"
+  sq <- sqnc
+  return ([s]++sq)
   <|>
-  do {
-    symbol "-";
-    e <- aExpr;
-    return (t - e);
-    }
-  <|> return t;
-  }
+  do 
+    s <- stm 
+    symbol ";"
+    return ([s])
 
 
-term :: Parser Int
-term = do {
-  f <- factor;
-  do {
-    symbol "*";
-    t <- term;
-    return (f * t);
-    }
-  <|>
-  do{
-    symbol "/";
-    t <- term;
-    return (f `div` t);
-    }
-  <|>
-  return f;
-  }
 
 
-factor :: Parser Int
-factor = do {
-  ;symbol "("
-  ;e <- aExpr
-  ;symbol ")"
-  ;return e
-  }
-  <|> natural
-  <|> varValue
+comm :: Parser Tree
+comm = do 
+  var <- identifier
+  do symbol "="
+     e <- expr
+     return (AssignNode var e)
+  <|> 
+  do symbol "IF"
+     symbol "["
+     b <- expr
+     symbol "]"
+     s <-  program
+     return (CommandNode If b s)
+  <|> 
+  do symbol "WHILE"
+     symbol "["
+     b <- expr
+     symbol "]"
+     s <-  program
+     return (CommandNode While b s)
 
 
-bool:: Parser Bool
-bool = do{
-  do{
-    parseTrue;
-    return True;
-    }
-  <|>
-  do {
-    parseFalse;
-    return False;
-    }
-  <|>
-  do {
-    num1 <- aExpr;
-    symbol "<";
-    num2 <- aExpr;
-    if num1 < num2 then return True else return False;
-    }
-  <|>
-  do {
-    num1 <- aExpr;
-    symbol "<=";
-    num2 <- aExpr;
-    if num1 <= num2 then return True else return False;
-    }
-  <|>
-  do{
-    num1 <- aExpr;
-    symbol ">";
-    num2 <- aExpr;
-    if num1 > num2 then return True else return False;
-    }
-  <|>
-  do {
-    num1 <- aExpr;
-    symbol ">=";
-    num2 <- aExpr;
-    if num1 >= num2 then return True else return False;
-    }
-  <|>
-  do{
-    num1 <- aExpr;
-    symbol "==";
-    num2 <- aExpr;
-    if num1 == num2 then return True else return False;
-    }
-  <|>
-  do{
-    symbol "not";
-    symbol "(";
-    t1 <- bool;
-    symbol ")";
-    return (not t1);
-    }
-  }
+-----------------------------------------------------------------------------
+
+expr :: Parser Tree
+expr = do
+  t <- term
+  do symbol "+"
+     e <- expr
+     return (SumNode Plus  t e)
+   <|> do symbol "-"
+          e <- expr
+          return (SumNode Minus  t e)
+   <|> do symbol "AND"
+          e <- expr
+          return (LogicNode And t e)
+   <|> do symbol "OR"
+          e <- expr
+          return (LogicNode And t e)
+   <|> do symbol "GREATER"
+          e <- expr
+          return (ArithmeticLogicNode Greater t e)
+   <|> do symbol "GREATER_EQUAL"
+          e <- expr
+          return (ArithmeticLogicNode GreaterEqual t e)
+   <|> do symbol "LESS"
+          e <- expr
+          return (ArithmeticLogicNode Less t e)
+   <|> do symbol "LESS_EQUAL"
+          e <- expr
+          return (ArithmeticLogicNode LessEqual t e)
+   <|> do symbol "EQUAL"
+          e <- expr
+          return (ArithmeticLogicNode Equal t e)
+   <|> do symbol "NOT_EQUAL"
+          e <- expr
+          return (ArithmeticLogicNode NotEqual t e)
+   <|> return t
 
 
-bExpr :: Parser Bool
-bExpr = do{
-  do{
-    t1 <- bExprTemp;
-    symbol "and";
-    t2 <- bExprTemp;
-    return (t1 && t2);
-    }
-  <|>
-  do{
-    t1 <- bExprTemp;
-    symbol "or";
-    t2 <- bExprTemp;
-    return (t1 || t2);
-    }
-  <|>
-  do{
-    t1 <- bExprTemp;
-    return t1;
-    }
-  }
- 
-bExprTemp :: Parser Bool
-bExprTemp = do{
-  do{
-    symbol "(";
-    t1 <- bExprTemp;
-    symbol "and";
-    t2 <- bExprTemp;
-    symbol ")";
-    return (t1 && t2);
-    }
-  <|>
-  do{
-    symbol "(";
-    t1 <- bExprTemp;
-    symbol "or";
-    t2 <- bExprTemp;
-    symbol ")";
-    return (t1 || t2);
-    }
-  <|>
-  do{
-    symbol "(";
-    t1 <- bExprTemp;
-    symbol ")";
-    return t1;
-    }
-  <|>
-  do{
-    t1 <- bool;
-    return t1;
-    }
-  <|>
-  bExpr;
-  }
+
+term :: Parser Tree
+term = 
+  do aterm
+    <|>
+    do bterm
 
 
-updateEnv :: Char -> Int -> Parser String
-updateEnv k v = P (\env inp -> case inp of
-  xs -> [((modify env k v), "" ,xs)])
-
-modify :: Env -> Char -> Int -> Env
-modify [] k v = [(k,v)]
-modify (x:xs) k v = if x == k then [(k,v)] ++ xs else [x] ++ modify xs k v
-
-
-command :: Parser String
-command = do {
-  do{
-    parseSkip;
-    symbol ";";
-    }
-  <|>
-  do {
-    assignment;
-    symbol ";";
-    }
-  }
-
-assignment :: Parser String
-assignment = do{
-  var <- item;
-  symbol ":=";
-  num <- aExpr;
-  updateEnv var num;
-  }
+aterm :: Parser Tree
+aterm = do
+  f <- afactor
+  do symbol "*"
+     t <- aterm
+     return (ProdNode Times t f)
+   <|> do symbol "/"
+          t <- aterm
+          return (ProdNode Div t f)
+   <|> return f
 
 
-cExpr :: Parser String
-cExpr = do {
-  do{
-    command;
-    cExpr;
-    }
-  <|>
-  do{
-    symbol "if";
-    cond <- bExpr;
-    symbol "then";
-    if cond == True then do {
-      cExpr;
-      parseTilEndif 1;
-      }
-    else do{
-      do{
-        parseTilElse;
-        cExpr;
-        symbol "endif;";
-        }
-      <|>
-      do{
-        parseTilEndif 1;
-        }
-      };
-    cExpr;
-    }
-  <|>
-  do{
-    symbol "while";
-    copyWhile;
-    cond <- bExpr;
-    symbol "do";
-    if cond == True then do{
-      cExpr;
-      symbol "endwhile;";
-      cExpr;
-      }
-    else do {
-      deleteFirstCopy 1;
-      deleteSecondCopy 0;
-      cExpr;
-      };
-    }
-  <|>
-  do{
-    symbol "while";
-    copyWhile;
-    cond <- bExpr;
-    symbol "do";
-    if cond == True then do{
-      cExpr;
-      symbol "endwhile;";
-      cExpr;
-      }
-    else do {
-      deleteFirstCopy 1;
-      deleteSecondCopy 0;
-      };
-  }
-  <|>
-  do{
-    command;
-    }
-  } 
+afactor :: Parser Tree
+afactor = 
+  do symbol "("
+     e <- expr
+     symbol ")"
+     return e
+     <|> 
+  do symbol "+"
+     a <- afactor
+     return (UnaryBoolNode Plus a)
+     <|> 
+  do symbol "-"
+     a <- afactor
+     return (UnaryBoolNode Minus a)
+     <|> 
+  do 
+     id <-identifier
+     return (VarNode id)
+     <|> 
+  do 
+     n <- natural
+     return (NumNode n)
 
 
-eval :: Env -> String -> String
-eval env xs = case (parse cExpr env xs) of
-  [(env, n, [])] -> getMemory env
-  [(env, _, out)] -> error "unused input: " ++ out
-  [] -> error "Invalid input"
+
+bterm :: Parser Tree
+bterm = 
+  do symbol "("
+     e <- expr
+     symbol ")"
+     return e
+     <|> 
+  do symbol "NOT"
+     b <- bterm
+     return (UnaryBoolNode Not b)
+     <|> 
+  do 
+     id <-identifier
+     return (VarNode id)
+     <|> 
+  do 
+     b <- bool_val
+     return (BoolNode b)
+
+
+
+-- parse :: String -> Tree
+-- eval xs = case (parse program xs) of
+--   [(n, [])]  -> n
+--   [(_, out)] -> error ("Unused input " ++ out)
+--   []         -> error "Invalid input"
+
+-----------------------------------------------------------------------------
+
+type Memory = [(String, String)]
+
+
+--Reads the memory, if variables with the same name are in memory it reads the last updated value (STACK mode).
+lookUp :: (Eq a) => a -> [(a,b)] -> Maybe b
+lookUp _key [] = Nothing
+lookUp key ((x,y):xys)
+ | key == x = Just y
+ | otherwise = lookUp key xys
+
+--Executes a sequence of commands, returning the updated Memory. 
+execute :: Tree -> Memory -> Memory
+execute tr r = 
+   case tr of
+      (SeqNode []) -> r
+      (SeqNode (s : ss)) -> execute (SeqNode ss) (execute s r)
+      (StatementNode e) -> execute e r
+      (AssignNode s e) -> (s, evaluate e r) : r
+      (CommandNode If b st) -> 
+         let cond = read(evaluate b r) in 
+            if cond /= False then execute st r else r
+      (CommandNode While b s) -> 
+         let cond = read(evaluate b r) in 
+            if cond /= False then execute (SeqNode [s,CommandNode While b s]) r else r
+      _ -> 
+        ("stdOut", evaluate tr r) : r
+
+
+{-Evaluates the different Trees (logic and arithmetic), converting the value received by the type specific functions (boolean or integer) into
+  a String storable in Memory-}
+evaluate :: Tree -> Memory -> String 
+--arithmetic
+evaluate (NumNode n) r = show (a_evaluate (NumNode n) r)
+evaluate (SumNode Plus e1 e2) r = show (a_evaluate (SumNode Plus e1 e2) r) 
+evaluate (SumNode Minus e1 e2) r = show (a_evaluate (SumNode Minus e1 e2) r)
+evaluate (UnaryNode Plus e1) r = show (a_evaluate (UnaryNode Plus e1) r )
+evaluate (UnaryNode Minus e1) r = show (a_evaluate (UnaryNode Minus e1) r)
+evaluate (ProdNode Times e1 e2) r = show (a_evaluate (ProdNode Times e1 e2) r) 
+evaluate (ProdNode Div e1 e2) r = show (a_evaluate (ProdNode Div e1 e2) r) 
+--bool
+evaluate (BoolNode b) r = show(b_evaluate (BoolNode b) r)
+evaluate (LogicNode And e1 e2) r = show(b_evaluate (LogicNode And e1 e2) r)
+evaluate (LogicNode Or e1 e2) r = show(b_evaluate (LogicNode Or e1 e2) r)
+evaluate (UnaryBoolNode Not e1) r = show(b_evaluate (UnaryBoolNode Not e1) r)
+evaluate (ArithmeticLogicNode Greater e1 e2) r = show(b_evaluate (ArithmeticLogicNode Greater e1 e2) r)
+evaluate (ArithmeticLogicNode GreaterEqual e1 e2) r = show(b_evaluate (ArithmeticLogicNode GreaterEqual e1 e2) r)
+evaluate (ArithmeticLogicNode Less e1 e2) r = show(b_evaluate (ArithmeticLogicNode Less e1 e2) r)
+evaluate (ArithmeticLogicNode LessEqual e1 e2) r = show(b_evaluate (ArithmeticLogicNode LessEqual e1 e2) r)
+evaluate (ArithmeticLogicNode Equal e1 e2) r = show(b_evaluate (ArithmeticLogicNode Equal e1 e2) r)
+evaluate (ArithmeticLogicNode NotEqual e1 e2) r = show(b_evaluate (ArithmeticLogicNode NotEqual e1 e2) r)
+--var
+evaluate (VarNode x) r = case lookUp x r of
+  Nothing -> error ("unbound variable `" ++ x ++ "'")
+  Just v -> v
+
+--Evaluates arithmetic expressions returning the resulting integer value
+a_evaluate ::  Tree -> Memory -> Int
+a_evaluate (NumNode n) r = n
+a_evaluate (VarNode x) r = case lookUp x r of
+ Nothing -> error ("unbound variable `" ++ x ++ "'")
+ Just v -> read v
+a_evaluate (SumNode Plus e1 e2) r = a_evaluate e1 r + a_evaluate e2 r
+a_evaluate (SumNode Minus e1 e2) r = a_evaluate e1 r - a_evaluate e2 r
+a_evaluate (UnaryNode Plus e) r = 0 +( a_evaluate e r)
+a_evaluate (UnaryNode Minus e) r = 0 -(a_evaluate e r)
+a_evaluate (ProdNode Times e1 e2) r = a_evaluate e1 r * a_evaluate e2 r
+a_evaluate (ProdNode Div e1 e2) r = a_evaluate e1 r `div` a_evaluate e2 r
+
+--Evaluates logic expressions returning the resulting boolean value
+b_evaluate ::  Tree -> Memory -> Bool
+b_evaluate (BoolNode b) r = b
+b_evaluate (VarNode x) r = case lookUp x r of
+ Nothing -> error ("unbound variable `" ++ x ++ "'")
+ Just v -> read v
+b_evaluate (LogicNode And e1 e2) r = b_evaluate e1 r && b_evaluate e2 r
+b_evaluate (LogicNode Or e1 e2) r = b_evaluate e1 r || b_evaluate e2 r
+b_evaluate (ArithmeticLogicNode Greater e1 e2) r = a_evaluate e1 r > a_evaluate e2 r
+b_evaluate (ArithmeticLogicNode GreaterEqual e1 e2) r = a_evaluate e1 r >= a_evaluate e2 r
+b_evaluate (ArithmeticLogicNode Less e1 e2) r = a_evaluate e1 r < a_evaluate e2 r
+b_evaluate (ArithmeticLogicNode LessEqual e1 e2) r = a_evaluate e1 r <= a_evaluate e2 r
+b_evaluate (ArithmeticLogicNode Equal e1 e2) r = a_evaluate e1 r == a_evaluate e2 r
+b_evaluate (ArithmeticLogicNode NotEqual e1 e2) r = a_evaluate e1 r /= a_evaluate e2 r
+
+
+--rename of the String type in Program type
+type Program = String
+
+type VarName = String
+type ProgramInput = String
+type ProgramOutput = String
+
+run :: Program -> Memory -> Memory
+run p env = case (parse program p) of
+  [(t, [])]  -> execute (t) env
+  [(_, out)] -> error ("Unused input " ++ out)
+  []         -> error "Invalid input"
+
